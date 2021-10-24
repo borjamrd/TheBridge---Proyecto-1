92c56e80e9250c8796015d2afab280fb49fc5fd7
patriglezperez <patriciaggonzalezperez@hotmail.com>
2021-10-24 20:11:35 +0200

    generate groups button

--- proyecto_1.js
+++ proyecto_1.js
@@ -5,14 +5,14 @@ let names = [];
 
 function addName() {
     let name = document.getElementById("name").value;
-    
-    if (name == ""){
+
+    if (name == "") {
         alert("Chato, te falta el nombre");
     } else {
         names.push(name);
         let textarea = document.getElementById("people");
         textarea.value = names.join("\n");
-        console.log(names);
+        //console.log(names);
     }
 }
 
@@ -22,20 +22,74 @@ function deleteName() {
     let textarea = document.getElementById("people");
     let name = document.getElementById("name");
     textarea.value = "";
-    name.value ="";
+    name.value = "";
 }
 /*------ Función generate ------*/
-function generate(){
-    let number = document.getElementById("teamSize").value
-    if ((number > names.length - 1) || (number < 2)){
-        alert("Escribe un número entre dos (incluido) y el número de nombres añadidos")
-    }else{
-        console.log(number)
+let newTeam = [];
+function createTeams() {
+    let numberOfTeams = parseInt(document.getElementById("teamSize").value); // cogemos el valor de los equipos
+    
+    if (numberOfTeams < 2 || numberOfTeams > names.length) {
+        alert(`Introduce un valor entre 2 y ...`); //falta el contador
+    } else {
+        while (names.length > 0) {
+            for (let i = 0; i < names.length; i++) {
+                let random = Math.floor(Math.random()*(names.length-1)); // conseguimos un nombre aleatorio                
+                let selection = names[random]; // lo selecciona
+                newTeam.push([i,selection]) // lo mete como indice
+                //console.log(newTeam);
+                names.splice(random, 1); // borra el que ha escogido
+            }
+        }
+        console.log(newTeam)
+        
+        // Numeramos los equipos formados y los formamos
+        for (let numb = 0; numb < numberOfTeams; numb++) {
+            console.log(`Team number ${numb + 1}`);
+            for (let m = numb; m < newTeam.length; m += numberOfTeams) {
+                if (newTeam[m][1] != undefined) {    
+                    console.log(newTeam[m][1]);
+                    break;
+                }
+            }
+        }
+            
+        for (let i = 0; i < numberOfTeams; i++) {
+            let container = document.getElementById("teams");
+
+            //Creamos div 
+            let div = document.createElement("div");
+            div.id = "newDiv";
+            div.classList = "new-div";
+            let boxDiv = document.getElementById("n-team");
+            container.appendChild(boxDiv);
+
+            //Creamos h3 (titulos de los equipos)
+            let titleTeam = document.createElement("h3");
+            titleTeam.id = "newTitle";
+            titleTeam.classList = "new-title";
+            titleTeam.innerHTML = `Team number ${i + 1}`;
+            let boxTitle = document.getElementById("h3-team");
+            boxTitle.appendChild(titleTeam);
+
+            //Creamos p de cada nombre
+            for (let j = i; j < newTeam.length; j += numberOfTeams) {
+                if (newTeam[j][1] != undefined) {
+                    let individual = document.createElement("p");
+                    individual.id = "newIndividual"
+                    individual.classList = "new-individual"
+                    individual.innerHTML = newTeam[j][1];
+                    boxTitle.appendChild(individual);
+                }
+            }
+        }
     }
-    /*PATRICIA: que tal, me falta hacer la división en equipos, lo ideal es introducirlo en "n-team"*/
 }
-
+console.log(createTeams())
 /*------ Función reset ------*/
-function reset(){
+function reset() {
 
 }
+
+
+
