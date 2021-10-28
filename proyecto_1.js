90628e86a3048bab343f6cbce11dc78c85de4f17
santiago mezquita <santimezquita@hotmail.es>
2021-10-28 06:58:14 +0200

    ya con estilos a los equipos creados, botón reset funcionando

--- proyecto_1.js
+++ proyecto_1.js
@@ -12,14 +12,13 @@ function addName() {
         names.push(name);
         let textarea = document.getElementById("people");
         textarea.value = names.join("\n");
-        console.log(names);
         document.getElementById("name").value = "";
         document.getElementById("name").focus();
     }
 }
 
-/*------ Función delete all ------*/
-function deleteAll() {
+/*------ Función delete all------*/
+function deleteName() {
     names = [];
     let textarea = document.getElementById("people");
     let name = document.getElementById("name");
@@ -27,60 +26,56 @@ function deleteAll() {
     name.value = "";
 }
 
-/*-------Fucntion delete-------*/
+/*------ Function delete -----*/
 function deleteMember() {
     names.pop();
     let textarea = document.getElementById("people");
     textarea.value = names.join("\n");
 }
+
 /*------ Función generate ------*/
 let newTeam = [];
 function createTeams() {
     let numberOfTeams = parseInt(document.getElementById("teamSize").value); // cogemos el valor de los equipos
-    
     if (numberOfTeams < 2 || numberOfTeams > names.length) {
         alert(`Introduce un valor entre 2 y ...`); //falta el contador
     } else {
         while (names.length > 0) {
-            for (let i = 0; i < names.length; i++) {
-                let random = Math.floor(Math.random()*(names.length-1)); // formula aleatoria             
-                let selection = names[random]; // lo selecciona un nombre del array names [posicion random]
-                newTeam.push([i,selection]) // vamos a meter la posicion i y el nombre random 
-                //console.log(newTeam);
-                names.splice(random, 1); // borra el que hemos metido
-            }
+            /* for (let i = 0; i < names.length; i++) { -----no le veo sentido a este for, con el while ya recorre todo*/
+            let random = Math.floor(Math.random()*(names.length-1)); // formula aleatoria             
+            let selection = names[random]; // lo selecciona un nombre del array names [posicion random]
+            newTeam.push([selection]) // vamos a meter la posicion i y el nombre random //al quitar el for no hace falta
+            names.splice(random, 1); // borra el que hemos metido
+            /* } */
         }
-        //console.log(newTeam) comprobamos que se meten en el array  
-            
+
         for (let i = 0; i < numberOfTeams; i++) {
             let container = document.getElementById("teams");
 
             //Creamos div 
             let div = document.createElement("div");
-            div.id = "newDiv";
+            div.id = "newDiv";//no tiene sentido darle el mismo id a todos los div, para dar estilos tenemos la clase y los id debería ser únicos
             div.classList = "new-div";
             container.appendChild(div);
 
             //Creamos h3 (titulos de los equipos)
             let titleTeam = document.createElement("h3");
-            titleTeam.id = "newTitle";
+            titleTeam.id = "newTitle";//lo mismo con este id
             titleTeam.classList = "new-title";
             titleTeam.innerHTML = `GRUPO ${i + 1}`;
             div.appendChild(titleTeam);
 
             //Creamos p de cada nombre
             for (let j = i; j < newTeam.length; j += numberOfTeams) {
-                    let individual = document.createElement("p");
-                    individual.id = "newIndividual"
-                    individual.classList = "new-individual"
-                    individual.innerHTML = newTeam[j][1]; //si no ponemos el 1 nos dice el valor
-                    div.appendChild(individual);
-               
+                let individual = document.createElement("p");
+                individual.id = "newIndividual"; //lo mismo con este id
+                individual.classList = "new-individual";
+                individual.innerHTML = newTeam[j]; //si no ponemos el 1 nos dice el valor
+                div.appendChild(individual);
             }
         }
     }
 }
-console.log(createTeams())
 /*------ Función reset ------*/
 function reset() {
     let container = document.getElementById("teams");
@@ -88,8 +83,8 @@ function reset() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
+    names = newTeam; //reinicializamos el array con los nombres de los participantes para poder volver a generar equipos
     newTeam = [];
-    //console.log(newTeam)
 }
 
 
