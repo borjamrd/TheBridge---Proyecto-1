6e1e6a49d4845eb17da46f4da2d6d2c145775de3
santiago mezquita <santimezquita@hotmail.es>
2021-10-29 06:41:27 +0200

    Contador ya mostrado en pantalla, botones de Delete y Delete All actualizados y controlado que no se introduzcan dos nombres iguales.

--- proyecto_1.js
+++ proyecto_1.js
@@ -1,25 +1,37 @@
 /* --Declaramos el array para acumular los nombres */
 let names = [];
-
+let count = 0;
 /*------ Funcion addName ------*/
 
-function addName() {
+function addName(event) {
+    
     let name = document.getElementById("name").value;
-
     if (name == "") {
         alert("Chato, te falta el nombre");
-    } else {
-        names.push(name);
-        let textarea = document.getElementById("people");
-        textarea.value = names.join("\n");
-        document.getElementById("name").value = "";
         document.getElementById("name").focus();
+    } else if ((event.type == "click") || (event.keyCode === 13)) {
+        if (names.includes(name)) {
+            alert("Upps, parece que ese nombre ya está en la lista, introduce otro nombre"); 
+        }
+        else {
+            names.push(name);
+            count ++;
+            document.getElementById('counter').value = count;
+            let textarea = document.getElementById("people");
+            textarea.value = names.join("\n");
+            console.log(names);
+            document.getElementById("name").value = "";
+            document.getElementById("name").focus();
+        }
+
     }
 }
 
-/*------ Función delete all------*/
+/*------ Función delete ------*/
 function deleteName() {
     names = [];
+    count = 0;
+    document.getElementById('counter').value = count;
     let textarea = document.getElementById("people");
     let name = document.getElementById("name");
     textarea.value = "";
@@ -29,53 +41,59 @@ function deleteName() {
 /*------ Function delete -----*/
 function deleteMember() {
     names.pop();
+    count --;
+    document.getElementById('counter').value = count;
     let textarea = document.getElementById("people");
     textarea.value = names.join("\n");
 }
-
 /*------ Función generate ------*/
 let newTeam = [];
 function createTeams() {
     let numberOfTeams = parseInt(document.getElementById("teamSize").value); // cogemos el valor de los equipos
-    if (numberOfTeams < 2 || numberOfTeams > names.length) {
-        alert(`Introduce un valor entre 2 y ...`); //falta el contador
+    /* let count = names.length */
+    if (numberOfTeams < 2 || numberOfTeams > (count/2)) {
+        alert("Introduce un valor entre 2 y "+ parseInt(count/2)); //falta el contador
     } else {
         while (names.length > 0) {
-            /* for (let i = 0; i < names.length; i++) { -----no le veo sentido a este for, con el while ya recorre todo*/
-            let random = Math.floor(Math.random()*(names.length-1)); // formula aleatoria             
-            let selection = names[random]; // lo selecciona un nombre del array names [posicion random]
-            newTeam.push([selection]) // vamos a meter la posicion i y el nombre random //al quitar el for no hace falta
-            names.splice(random, 1); // borra el que hemos metido
+            /* for (let i = 0; i < names.length; i++) { */
+                let random = Math.floor(Math.random()*(names.length-1)); // formula aleatoria             
+                let selection = names[random]; // lo selecciona un nombre del array names [posicion random]
+                newTeam.push([selection]) // vamos a meter la posicion i y el nombre random 
+                //console.log(newTeam);
+                names.splice(random, 1); // borra el que hemos metido
             /* } */
         }
-
+        //console.log(newTeam) comprobamos que se meten en el array  
+            
         for (let i = 0; i < numberOfTeams; i++) {
             let container = document.getElementById("teams");
 
             //Creamos div 
             let div = document.createElement("div");
-            div.id = "newDiv";//no tiene sentido darle el mismo id a todos los div, para dar estilos tenemos la clase y los id debería ser únicos
+            div.id = "newDiv";
             div.classList = "new-div";
             container.appendChild(div);
 
             //Creamos h3 (titulos de los equipos)
             let titleTeam = document.createElement("h3");
-            titleTeam.id = "newTitle";//lo mismo con este id
+            titleTeam.id = "newTitle";
             titleTeam.classList = "new-title";
             titleTeam.innerHTML = `GRUPO ${i + 1}`;
             div.appendChild(titleTeam);
 
             //Creamos p de cada nombre
             for (let j = i; j < newTeam.length; j += numberOfTeams) {
-                let individual = document.createElement("p");
-                individual.id = "newIndividual"; //lo mismo con este id
-                individual.classList = "new-individual";
-                individual.innerHTML = newTeam[j]; //si no ponemos el 1 nos dice el valor
-                div.appendChild(individual);
+                    let individual = document.createElement("p");
+                    individual.id = "newIndividual"
+                    individual.classList = "new-individual"
+                    individual.innerHTML = newTeam[j]; //si no ponemos el 1 nos dice el valor
+                    div.appendChild(individual);
+               
             }
         }
     }
 }
+console.log(createTeams())
 /*------ Función reset ------*/
 function reset() {
     let container = document.getElementById("teams");
@@ -83,9 +101,7 @@ function reset() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    names = newTeam; //reinicializamos el array con los nombres de los participantes para poder volver a generar equipos
+    names = newTeam;
     newTeam = [];
+    //console.log(newTeam)
 }
-
-
-
